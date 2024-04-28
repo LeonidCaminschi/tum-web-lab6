b03f68c659c03be4aec24abbf2f94e37dddc4cb4
LeonidCaminschi <leocer.leonid@gmail.com>
2024-04-29 02:41:24 +0300

    Added logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import logo from './Blockbuster_logo.svg.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
 }
