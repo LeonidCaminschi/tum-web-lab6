bb861fb879507494c8d00b79fa5ad2a8b764230a
LeonidCaminschi <leocer.leonid@gmail.com>
2024-04-29 02:55:32 +0300

    Added movie container and repaired logo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ function App() {
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
+      <div className="movie-container">
+      <div className="movie">
+          <img src="movie1.jpg" alt="Movie 1" />
+          <h2>Movie 1 Title</h2>
+        </div>
+        <div className="movie">
+          <img src="movie2.jpg" alt="Movie 2" />
+          <h2>Movie 2 Title</h2>
+        </div>
+      </div>
     </div>
   );
 }
