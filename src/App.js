cdf1ad6ffc6e1b6ec82773a109953d8d7125cacb
LeonidCaminschi <leocer.leonid@gmail.com>
2024-04-29 03:06:52 +0300

    Semi working Movie Entities

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './Blockbuster_logo.svg.png';
+import movielogo from './hitman.jpg'
 import './App.css';
 
 function App() {
@@ -7,11 +8,11 @@ function App() {
       <img src={logo} className="App-logo" alt="logo" />
       <div className="movie-container">
       <div className="movie">
-          <img src="movie1.jpg" alt="Movie 1" />
+          <img src={movielogo} alt="Movie 1" />
           <h2>Movie 1 Title</h2>
         </div>
         <div className="movie">
-          <img src="movie2.jpg" alt="Movie 2" />
+          <img src={movielogo} alt="Movie 2" />
           <h2>Movie 2 Title</h2>
         </div>
       </div>
