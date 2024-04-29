3af77c4271a17b7f2dc619732fb1e3da9c0b751a
LeonidCaminschi <leocer.leonid@gmail.com>
2024-04-29 03:37:52 +0300

    Implemented dynamic movie vizualization

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import logo from './Blockbuster_logo.svg.png';
-import movielogo from './hitman.jpg'
 import './App.css';
 
 function App() {
+  const movies = [
+    { title: 'Hitman', img: 'hitman.jpg' },
+    { title: 'Avatar', img: 'avatar.jpg' },
+    // add more movies here
+  ];
+
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
       <div className="movie-container">
-      <div className="movie">
-          <img src={movielogo} alt="Movie 1" />
-          <h2>Movie 1 Title</h2>
-        </div>
-        <div className="movie">
-          <img src={movielogo} alt="Movie 2" />
-          <h2>Movie 2 Title</h2>
-        </div>
+        {movies.map((movie, index) => (
+          <div className="movie" key={index}>
+            <img src={require(`./images/${movie.img}`)} alt={movie.title} />
+            <h2>{movie.title}</h2>
+          </div>
+        ))}
       </div>
     </div>
   );
